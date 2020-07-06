6169038b7b7fe6c06594f0a4da94a3f9531e1c2d
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-07-06 00:32:09 -0300

    :sparkles: Adding routes and challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -15,14 +20,38 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.filter(transaction => {
+      return transaction.type === 'income';
+    });
+    const outcome = this.transactions.filter(transaction => {
+      return transaction.type === 'outcome';
+    });
+    const totalIncome = income.reduce(
+      (accumulator, { value }) => Number(accumulator) + Number(value),
+      0,
+    );
+    const totalOutcome = outcome.reduce(
+      (accumulator, { value }) => Number(accumulator) + Number(value),
+      0,
+    );
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +13,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: RequestDTO): Transaction {
     // TODO
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error('You do not have enough balance');
+    }
+
+    if (type === 'income' || type === 'outcome') {
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      });
+      return transaction;
+    }
+
+    throw Error('Type not found, try income or outcome');
   }
+  // Fiz assim pq eu não consegui fazer o contrário, que seria throw Error() dentro do if com comparação de !==
 }
 
 export default CreateTransactionService;
